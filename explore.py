7ec5a4bf7f99f38217051121474bb18081f462d1
Omar Ocegueda <jomaroceguedag@gmail.com>
2015-11-17 12:36:28 -0600

    Add initial segmentation

--- explore.py
+++ explore.py
@@ -10,6 +10,12 @@ from dipy.align.imaffine import (AffineMap,
                                  AffineRegistration)
 from dipy.align.imwarp import (SymmetricDiffeomorphicRegistration)
 from dipy.align.metrics import (CCMetric)
+from scipy.ndimage.morphology import binary_dilation
+
+try:
+    from experiments.registration.regviz import overlay_slices_with_contours
+except:
+    from dipy.viz.regtools import overlay_slices as overlay_slices_with_contours
 
 def dipy_align(static, static_grid2world, moving, moving_grid2world):
     r''' Full rigid registration with Dipy's imaffine module
@@ -62,34 +68,34 @@ def dipy_align(static, static_grid2world, moving, moving_grid2world):
     
     return sol
 
-names = ['infant-neo.nii', 'infant-neo-aal.nii', 'infant-neo-avgseg.nii', 'infant-neo-seg.nii']
-neo_nib = nib.load('infant-neo.nii')
-neo = neo_nib.get_data()
-neo_affine = neo_nib.get_affine()
-rt.plot_slices(neo)
+def check_neonate_atlas():
+    names = ['infant-neo.nii', 'infant-neo-aal.nii', 'infant-neo-avgseg.nii', 'infant-neo-seg.nii']
+    neo_nib = nib.load('infant-neo.nii')
+    neo = neo_nib.get_data()
+    neo_affine = neo_nib.get_affine()
+    rt.plot_slices(neo)
 
-neo_aal_nib = nib.load('infant-neo-aal.nii')
-neo_aal = neo_aal_nib.get_data()
-neo_aal_affine = neo_aal_nib.get_affine()
-rt.plot_slices(neo_aal)
+    neo_aal_nib = nib.load('infant-neo-aal.nii')
+    neo_aal = neo_aal_nib.get_data()
+    neo_aal_affine = neo_aal_nib.get_affine()
+    rt.plot_slices(neo_aal)
 
-neo_avgseg_nib = nib.load('infant-neo-avgseg.nii')
-neo_avgseg = neo_avgseg_nib.get_data()
-neo_avgseg_affine = neo_avgseg_nib.get_affine()
-rt.plot_slices(neo_avgseg)
+    neo_avgseg_nib = nib.load('infant-neo-avgseg.nii')
+    neo_avgseg = neo_avgseg_nib.get_data()
+    neo_avgseg_affine = neo_avgseg_nib.get_affine()
+    rt.plot_slices(neo_avgseg)
 
-neo_with_cerebellum_nib = nib.load('infant-neo-withCerebellum.nii')
-neo_with_cerebellum = neo_with_cerebellum_nib.get_data()
-neo_with_cerebellum_affine = neo_with_cerebellum_nib.get_affine()
-rt.plot_slices(neo_with_cerebellum)
+    neo_with_cerebellum_nib = nib.load('infant-neo-withCerebellum.nii')
+    neo_with_cerebellum = neo_with_cerebellum_nib.get_data()
+    neo_with_cerebellum_affine = neo_with_cerebellum_nib.get_affine()
+    rt.plot_slices(neo_with_cerebellum)
 
-neo_with_skull_nib = nib.load('infant-neo-withSkull.nii')
-neo_with_skull = neo_with_skull_nib.get_data()
-neo_with_skull_affine = neo_with_skull_nib.get_affine()
-rt.plot_slices(neo_with_skull)
+    neo_with_skull_nib = nib.load('infant-neo-withSkull.nii')
+    neo_with_skull = neo_with_skull_nib.get_data()
+    neo_with_skull_affine = neo_with_skull_nib.get_affine()
+    rt.plot_slices(neo_with_skull)
 
 # Test with adult brains
-
 # Load ibsr
 ibsr1_name = info.get_ibsr(1, 'raw')
 ibsr1_nib = nib.load(ibsr1_name)
@@ -97,6 +103,12 @@ ibsr1 = ibsr1_nib.get_data().squeeze()
 ibsr1_affine = ibsr1_nib.get_affine()
 
 # Load brainweb (and match ibsr axes)
+brainweb_strip_name = info.get_brainweb('t1','strip')
+brainweb_strip_nib = nib.load(brainweb_strip_name)
+brainweb_strip = brainweb_strip_nib.get_data().squeeze()
+brainweb_strip = brainweb_strip.transpose([0,2,1])[::-1,:,:]
+brainweb_mask = brainweb_strip>0
+
 brainweb_name = info.get_brainweb('t1','raw')
 brainweb_nib = nib.load(brainweb_name)
 brainweb = brainweb_nib.get_data().squeeze()
@@ -106,6 +118,8 @@ rt.plot_slices(brainweb)
 brainweb_affine = ibsr1_affine.copy()
 brainweb_affine[brainweb_affine!=0] = 1
 brainweb_affine[0,0] = -1
+
+
 
 # Reslice Brainweb on IBSR1
 ibsr_to_bw = AffineMap(None, ibsr1.shape, ibsr1_affine, brainweb.shape, brainweb_affine)
@@ -129,16 +143,39 @@ rt.overlay_slices(ibsr1, bw_on_ibsr1, slice_type=1) # aligned (axial view)
 rt.overlay_slices(ibsr1, bw_on_ibsr1, slice_type=2) # aligned (coronal view)
 
 # Start diffeomorphic registration
-metric = CCMetric(3)
-level_iters = [100, 100, 10]
-sdr = SymmetricDiffeomorphicRegistration(metric, level_iters)
-sdr.verbosity = VerbosityLevels.DEBUG
-ibsr_bw_diffmap = sdr.optimize(ibsr1, brainweb, ibsr1_affine, brainweb_affine, ibsr_bw_affmap.affine)
-bw_warped_ibsr1 = ibsr_bw_diffmap.transform(brainweb)
+diff_name = 'ibsr1_to_brainweb_diff.p'
+if os.path.isfile(diff_name):
+    ibsr_bw_diffmap = pickle.load(open(diff_name,'r'))
+else:
+    metric = CCMetric(3)
+    level_iters = [100, 10]
+    sdr = SymmetricDiffeomorphicRegistration(metric, level_iters)
+    sdr.verbosity = VerbosityLevels.DEBUG
+    ibsr_bw_diffmap = sdr.optimize(ibsr1, brainweb, ibsr1_affine, brainweb_affine, ibsr_bw_affmap.affine)
+    pickle.dump(ibsr_bw_diffmap, open(diff_name,'w'))
 
-rt.overlay_slices(ibsr1, bw_warped_ibsr1, slice_type=0) # warped (sagital view)
-rt.overlay_slices(ibsr1, bw_warped_ibsr1, slice_type=1) # warped (axial view)
-rt.overlay_slices(ibsr1, bw_warped_ibsr1, slice_type=2) # warped (coronal view)
+bw_warped_ibsr1 = ibsr_bw_diffmap.transform(brainweb)
+overlay_slices_with_contours(ibsr1, bw_warped_ibsr1, slice_type=0) # warped (sagital view)
+overlay_slices_with_contours(ibsr1, bw_warped_ibsr1, slice_type=1) # warped (axial view)
+overlay_slices_with_contours(ibsr1, bw_warped_ibsr1, slice_type=2) # warped (coronal view)
+
+# Now the initial segmentation
+bw_mask_ibsr1 = ibsr_bw_diffmap.transform(brainweb_mask)
+bw_mask_ibsr1 = bw_mask_ibsr1>0
+
+# Dilate
+structure = np.ones((5,5,5))
+dilated_mask = binary_dilation(bw_mask_ibsr1, structure)
+
+ibsr1_strip_init = ibsr1 * dilated_mask
+rt.plot_slices(ibsr1_strip_init)
+overlay_slices_with_contours(ibsr1, ibsr1_strip_init, slice_type=0) # warped (sagital view)
+overlay_slices_with_contours(ibsr1, ibsr1_strip_init, slice_type=1) # warped (axial view)
+overlay_slices_with_contours(ibsr1, ibsr1_strip_init, slice_type=2) # warped (coronal view)
+
+rt.overlay_slices(ibsr1, ibsr1_strip_init, slice_type=0) # warped (sagital view)
+rt.overlay_slices(ibsr1, ibsr1_strip_init, slice_type=1) # warped (axial view)
+rt.overlay_slices(ibsr1, ibsr1_strip_init, slice_type=2) # warped (coronal view)
 
 
 
